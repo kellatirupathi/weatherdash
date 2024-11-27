79bdbbe94e6ced6f4a9357819cd90ad678568f8e
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-11-27 16:55:09 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
-// src/utils/api.js
 import axios from 'axios';
 
-const API_KEY = 'f3e60d91d22421f77ca5bac8d290240e'; // Replace with your OpenWeatherMap API key
-const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+const API_KEY = '6bb2d0103d7a4aee99d103054242711';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 
-export const fetchWeatherData = (city) => {
-  return axios.get(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
-};
-
-export const fetchForecastData = (city) => {
-  return axios.get(`${BASE_URL}/forecast?q=${city}&units=metric&appid=${API_KEY}`);
+export const fetchWeatherData = (city, unit = 'metric') => {
+  return axios.get(`${BASE_URL}/current.json`, {
+    params: {
+      key: API_KEY,
+      q: city,
+    },
+  });
 };
