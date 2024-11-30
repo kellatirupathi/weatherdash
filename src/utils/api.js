54bcda7e10a19e91056a4b059e7250a0d51dd4ca
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-11-30 11:51:56 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,10 @@
-import axios from 'axios';
-
 const API_KEY = '6bb2d0103d7a4aee99d103054242711';
 const BASE_URL = 'https://api.weatherapi.com/v1';
-
+ 
 export const fetchWeatherData = (city, unit = 'metric') => {
-  return axios.get(`${BASE_URL}/current.json`, {
-    params: {
-      key: API_KEY,
-      q: city,
-    },
-  });
+  return fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}&units=${unit}`)
+    .then(response => response.json())
+    .catch(err => {
+      throw new Error('Error fetching weather data');
+    });
 };
